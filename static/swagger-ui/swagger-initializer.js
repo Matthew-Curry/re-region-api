04fb63fa4c27dbe84fb28b4005657ad6d887565c
Matthew Curry <matt.curry56@gmail.com>
2022-07-21 14:31:23 -0400

    open API specification responds

--- static/swagger-ui/swagger-initializer.js
+++ static/swagger-ui/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "http://127.0.0.1:8080/docs/",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
