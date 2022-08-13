84e04b6b58d2fffcd8e9e60d6c4a48efcaabf407
Matthew Curry <matt.curry56@gmail.com>
2022-08-13 17:05:23 -0400

    user_data, go web server can only read and execute

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -16,36 +16,36 @@ CERT_DIR="$CERT_DIR/cmd"
 
 # aws cli
 logMessage "Installing AWS CLI"
-sudo apt-get update
-sudo apt-get install awscli
+apt-get update
+apt-get install awscli
 
 # install docker
 logMessage "Installing Docker"
-sudo apt-get install \
+apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # postgres install
 logMessage "Installing Postgres"
-sudo apt install postgresql-client-common
-sudo apt-get install postgresql-client
+apt install postgresql-client-common
+apt-get install postgresql-client
 
 # to run docker without sudo
 logMessage "Adding Ubuntu user to the docker group"
-sudo groupadd docker
-sudo usermod -aG docker $USER
+groupadd docker
+usermod -aG docker $USER
 newgrp docker 
 
 # pull latest docker images
@@ -54,22 +54,20 @@ logMessage "Pulling latest docker images for the application"
 # pull needed files to run app
 logMessage "Pulling files needed to run the app from S3"
 cd $CMD_DIR
-sudo aws s3 cp s3://re-region/cmd/docker-compose.yml .
-sudo aws s3 cp s3://re-region/cmd/run.sh .
-
-# make the directories
-logMessage "Making directories to store the code"
-sudo mkdir $CODE_DIR
-sudo mkdir $APP_DIR
-sudo mkdir $CMD_DIR
-sudo mkdir $CERT_DIR
+touch pull_files.sh
+chmod 700 pull_files.sh
+echo 'sudo aws s3 cp s3://re-region/cmd . --rec' >> pull_files.sh
+./pull_files.sh
 
 # start the app
 logMessage "Starting the app"
+chmod 700 run_server.sh
+./run_server.sh
+
 
 # setup certs
 logMessage "Setting up the certs"
-sudo certbot certonly --webroot -w certs \ 
+certbot certonly --webroot -w certs \ 
               --preferred-challenges http \
               -d reregion.com -d www.reregion.com \ 
               --non-interactive --agree-tos -m $EMAIL
